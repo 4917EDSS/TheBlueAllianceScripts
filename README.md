ab0bda275bc18d78394d596e3499195a209a6dec
Sam Maier <sam@sammaier.ca>
2017-02-23 23:55:32 -0500

    opr per game working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Development
 Set up gcloud sdk. Get Python App Engine SDK. In WSL, use command `/usr/lib/google-cloud-sdk/bin/dev_appserver.py --use_mtime_file_watcher=true app.yaml` to run. However, I run into strange SSL errors when running locally. To deploy, `sudo gcloud app deploy app.yaml --project opr4917`
+
+Python 2.7.9 or higher is recommended.
--- basic_solver.py
+++ basic_solver.py
@@ -1,4 +1,5 @@
 import requests
+headers={'X-TBA-App-Id' : 'frc4917:customOPRCalculator:1'}
 
 def get_alliance_stat(stats, alliance):
     total_stat = 0;
@@ -13,31 +14,50 @@ def prediction_percentage(stats, matches):
         redAlliance = match['alliances']['red']
         blueAlliance = match['alliances']['blue']
         if (redAlliance['score'] == blueAlliance['score']): continue
-        redAllianceStat = get_alliance_stat(stats, redAlliance);
-        blueAllianceStat = get_alliance_stat(stats, blueAlliance);
+        try:
+            redAllianceStat = get_alliance_stat(stats, redAlliance);
+            blueAllianceStat = get_alliance_stat(stats, blueAlliance);
+        except KeyError:
+            continue
 
         if ((redAllianceStat > blueAllianceStat) == (redAlliance['score'] > blueAlliance['score'])):
             correct += 1
         total += 1
 
-    return correct / float(total)
+    return correct, total
 
 
-def do_event(event_code, playoffs_only=True):
-    url = "https://www.thebluealliance.com/api/v2/event/" + event_code + "/matches"
-    headers={"X-TBA-App-Id" : "frc4917:customOPRCalculator:1"}
+def do_event(event_code, totals, playoffs_only=True):
+    url = 'https://www.thebluealliance.com/api/v2/event/' + event_code + '/stats'
     r = requests.get(url, headers=headers)
     stats_contents = r.json()
+    if not ('oprs' in stats_contents and stats_contents['oprs'] and stats_contents['ccwms']): return
 
-    url = "https://www.thebluealliance.com/api/v2/event/" + event_code + "/matches"
-    headers={"X-TBA-App-Id" : "frc4917:customOPRCalculator:1"}
+    url = 'https://www.thebluealliance.com/api/v2/event/' + event_code + '/matches'
     r = requests.get(url, headers=headers)
     matches_contents = r.json()
     if playoffs_only:
         matches_contents = [x for x in matches_contents if x['comp_level'] != 'qm']
 
 
-    print prediction_percentage(stats_contents, matches_contents)
+    correct, total = prediction_percentage(stats_contents['oprs'], matches_contents)
+    totals['opr_correct'] += correct
+    totals['num_games'] += total
+    correct, total = prediction_percentage(stats_contents['ccwms'], matches_contents)
+    totals['ccwm_correct'] += correct
 
 
-do_event(raw_input('Enter an event code'))
+for year in range(2002, 2005):
+    url = 'https://www.thebluealliance.com/api/v2/events/' + str(year)
+    r = requests.get(url, headers=headers)
+    events_contents = r.json()
+
+    totals = {'num_games': 0, 'opr_correct': 0, 'ccwm_correct': 0}
+    for event in events_contents:
+        print event['key']
+        do_event(event['key'], totals)
+
+    print year
+    print totals
+    print 'OPR ' + str(totals['opr_correct'] / float(totals['num_games']))
+    print 'CCWM ' + str(totals['ccwm_correct'] / float(totals['num_games']))
